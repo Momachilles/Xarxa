6dc369c38dddf8c2517f455f4666d182dc1a8d4f
David Alarcon Gallego <m_2056383@gcognizant.com>
2022-07-15 15:53:23 +0200

    Initial Commit

---
+++ Xarxa/Xarxa.h
@@ -0,0 +1,18 @@
+//
+//  Xarxa.h
+//  Xarxa
+//
+//  Created by m_2056383 on 15/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Xarxa.
+FOUNDATION_EXPORT double XarxaVersionNumber;
+
+//! Project version string for Xarxa.
+FOUNDATION_EXPORT const unsigned char XarxaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Xarxa/PublicHeader.h>
+
+
